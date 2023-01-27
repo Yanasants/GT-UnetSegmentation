ab29b38354762856ccd4580b7c30aa0f48ca4aa5
Yana Santos <59098432+Yanasants@users.noreply.github.com>
2023-01-27 10:58:06 -0300

    Create mean_fo.py

---
+++ mean_fo.py
@@ -0,0 +1,38 @@
+import glob 
+import numpy as np
+from mean_functions import dice_coef_EV, dice_coef_girino, dice_coef_lung, jaccard_coef, calcula_metricas
+from utils import load_images_array
+
+TEMPO = []
+ORIGINAL_SIZE = 4
+NEW_SIZE = 4
+for i in range(1,2):
+    # Choose train folder TM40 ou TM46
+    _folder = './imgs_testes_metricas/am_imgs' #teste_' + str(i)
+    # _folder = './dados_girino/TM46_40prod'
+    
+    GT_imgs = sorted(glob.glob(_folder + '/conjunto_a/*')) 
+    pred_imgs = sorted(glob.glob(_folder + '/conjunto_b/*'))
+    
+    gt = load_images_array(GT_imgs, new_size = NEW_SIZE)
+    pred = load_images_array(pred_imgs, new_size = NEW_SIZE)
+    
+    # VERSÃO EDUARDO E VITOR
+    dice = dice_coef_EV(gt, pred)
+    dice = str(dice).split(',')[0].split('(')[1]
+    print(f'Coeficiente Dice (Eduardo e Vitor): {dice}')
+    
+    
+    # VERSÃO ATUAL GIRINO
+    # Calcula iou e dice para todas as imagens deste fold (i)
+    dice_list = []
+    dice_list = calcula_metricas(gt, pred)  
+    
+    #estatística
+    mean = round(np.mean(dice_list), 4)
+    print(f'Coeficiente Dice (Versão do Laboratório): {mean}')
+    
+    # VERSÃO SEGMENTAÇÃO PULMÃO
+    dice = dice_coef_lung(gt, pred)
+    dice = str(dice).split(',')[0].split('(')[1]
+    print(f'Coeficiente Dice (Versão usada com as tomografias de pulmão): {dice}')
